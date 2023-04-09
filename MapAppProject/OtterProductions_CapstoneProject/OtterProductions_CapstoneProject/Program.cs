25b7d2118c36c5bc6d26c4d9a72d67aaeee39da3
AntoniaMMorales <brand@DESKTOP-ETMCMKC.(none)>
2023-04-09 14:55:36 -0700

    Fixing DB issues for login

--- MapAppProject/OtterProductions_CapstoneProject/OtterProductions_CapstoneProject/Program.cs
+++ MapAppProject/OtterProductions_CapstoneProject/OtterProductions_CapstoneProject/Program.cs
@@ -17,10 +17,10 @@ namespace OtterProductions_CapstoneProject
         {
             var builder = WebApplication.CreateBuilder(args);
             //var connectionString = builder.Configuration.GetConnectionString("AuthenticationConnection") ?? throw new InvalidOperationException("Connection string 'AuthenticationConnection' not found.");
-            var connectionString1 = builder.Configuration.GetConnectionString("AuthenticationConnectionAzure");
+            var connectionString = builder.Configuration.GetConnectionString("AuthenticationConnectionAzure");
 
 
-            builder.Services.AddDbContext<AuthenticationDbContext>(options => options.UseSqlServer(connectionString1));
+            builder.Services.AddDbContext<AuthenticationDbContext>(options => options.UseSqlServer(connectionString));
             //var builder= new SqlConnectionStringBuilder(Configuration.GetConnectionString("AuthenticationConnectionAzure"));
             //builder.Password = Configuration["OPAuth:DBPassword"];
             //var connectionString2 = builder.Configuration.GetConnectionString("ApplicationConnectionAzure");
